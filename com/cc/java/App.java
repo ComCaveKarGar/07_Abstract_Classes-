99882dc8323b8672d0101b8774b17a796b926bf5
Karsten <Moonjumper@gmx.de>
2023-05-30 13:00:48 +0200

    Mthem. Berechng. ergänzt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,5 +1,7 @@
 package com.cc.java;
 
+// import java.io.OutputStream;
+
 public class App {
     
     public static void main(String[] args) {
@@ -13,9 +15,25 @@ public class App {
         output("Fläche Dreieck: " + triangle.area());
         output("Fläche Kreis: " + circle.area());
 
+        output("------  Differenzen ------");
+        output("Diff. r-t: " + areaDiff(rectangle, triangle));
+        output("Diff. r-c: " + areaDiff(rectangle, circle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
+
     }
 
-  
+        private static double areaDiff(Shape s1, Shape s2) {
+            return s1.area() - s2.area();
+        }
+
+        
+    // private static double name(Rectangle r, Triangle t) {
+    //     return r.area() - t.area();
+        
+    // private static double name(Rectangle r, Circle t) {
+    //     return r.area() - t.area();
+    // }
 
    
 
