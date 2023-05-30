0a795c60ab1ba0d1ec5ea08b5437fcab37db20ce
Peter <schmidt.multimedia@gmail.com>
2023-05-30 12:14:42 +0200

    1. Iteration / Shape()+ Subklassen

---
+++ com/cc/java/Rectangle.java
@@ -0,0 +1,23 @@
+package com.cc.java;
+
+public class Rectangle extends Shape{
+
+    private double width,height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+
+
+
+
+
+    
+}
